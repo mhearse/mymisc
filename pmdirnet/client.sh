42818f1bfe706eb87ae9a71614bff922039f166d
mhearse <matt_hersant@yahoo.com>
2017-11-09 03:26:10 -0800

    Trivial syntax changes

--- pmdirnet/client.sh
+++ pmdirnet/client.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+OURIP=0.0.0.0
 OURPORT=1682
 
 for num in {1..10000}
 do
     KEY=$(uuid)
     VAL=$(openssl rand -hex 1024)
-    echo "put$KEY$VAL" | nc 0.0.0.0 $OURPORT
+    echo "put$KEY$VAL" | nc $OURIP $OURPORT
 done
--- pmdirnet/pmdirnet.sh
+++ pmdirnet/pmdirnet.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+OURIP=0.0.0.0
 OURPORT=1682
 OURDIR=/tmp/pmdirnet
 
 lsof -Pi :$OURPORT -sTCP:LISTEN 2>&1 > /dev/null || {
-    tcpserver -c 500 -HR 0.0.0.0 $OURPORT $(readlink -f $0)
+    tcpserver -c 500 -HR $OURIP $OURPORT $(readlink -f $0)
 }
 
 [ ! -d $OURDIR ] && mkdir $OURDIR
@@ -14,12 +15,13 @@ read -r INPUT
 echo $INPUT | 
 while IFS= read -r METHOD KEY VALUE
 do
-    DIRSUBSTR=$OURDIR/${KEY:0:4}
+    DIRSUBSTR=${KEY:0:4}
     case $METHOD in
-        put) [ ! -d $DIRSUBSTR ] && mkdir $DIRSUBSTR; echo $VALUE > $DIRSUBSTR/$KEY
+        put) [ ! -d $OURDIR/$DIRSUBSTR ] && mkdir $OURDIR/$DIRSUBSTR; echo $VALUE > $OURDIR/$DIRSUBSTR/$KEY
             ;;
-        get) cat $DIRSUBSTR/$KEY
+        get) cat $OURDIR/$DIRSUBSTR/$KEY
             ;;
+        del) unlink $OURDIR/$DIRSUBSTR/$KEY
     esac
 done
 
